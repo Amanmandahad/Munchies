c153c45a7c29a0e3b01f3cd8a3e61796098de393
Aman mandahad <mandahadaman2002@gmail.com>
2024-08-21 00:47:01 +0530

    started new

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import Home from './pages/home/Home';
-import './App.css'
-import Product from './pages/product/Product';
-import Products from './pages/products/Products';
-import {Routes,Route} from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer'
+import React from 'react'
+
 const App = () => {
   return (
     <div>
-
-      <Navbar/>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/products/:id" element={<Products />} />
-      </Routes>
-      <Footer/>
-
-
+ <h1>Url shortner</h1>
     </div>
   )
 }
